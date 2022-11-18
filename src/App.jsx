85845c5d7c38521709bef0082fb0b7635d0aa306
Remy349 <santoraga15@gmail.com>
2022-11-17 19:15:40 -0600

    Structure completed, need to add styles and form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import { Footer } from './components/Footer'
+import styled from 'styled-components'
+
+import logo from './images/logo.svg'
+import illustration from './images/illustration-dashboard.png'
+
+const ButtonSubmit = styled.button`
+  font-family: inherit;
+`
 
 function App() {
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <main className='main'>
-      <h1>OK!!!</h1>
-    </main>
+    <>
+      <main className='ping'>
+        <div className='ping__logo'>
+          <img className='ping__logo-img' src={logo} alt='Ping Logo' />
+        </div>
+        <div className='ping__content'>
+          <h1 className='ping__content-title'>
+            We are launching <span>soon!</span>
+          </h1>
+          <p className='ping__content-desc'>Subscribe and get notified</p>
+        </div>
+        <div className='ping__forms'>
+          <form onSubmit={handleOnSubmit} className='ping__form'>
+            <div className='ping__form-container'>
+              <input
+                type='text'
+                className='ping__form-input'
+                name='email'
+                placeholder='Your email address...'
+                autoComplete='off'
+              />
+            </div>
+            <ButtonSubmit className='ping__form-submit' type='submit'>
+              Notify Me
+            </ButtonSubmit>
+          </form>
+        </div>
+        <div className='ping__illustration'>
+          <img
+            className='ping__illustration-img'
+            src={illustration}
+            alt='Illustration'
+          />
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
+
+export const Footer = () => {
+  return (
+    <footer className='footer'>
+      <ul className='footer__list'>
+        <li className='footer__item'>
+          <a className='footer__link' href='#'>
+            <FaFacebookF />
+          </a>
+        </li>
+        <li className='footer__item'>
+          <a className='footer__link' href='#'>
+            <FaTwitter />
+          </a>
+        </li>
+        <li className='footer__item'>
+          <a className='footer__link' href='#'>
+            <FaInstagram />
+          </a>
+        </li>
+      </ul>
+      <p className='footer__copy'>© Copyright Ping. All rights reserved.</p>
+    </footer>
+  )
+}
